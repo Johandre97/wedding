84ed12ce035e5db75c7d68ec73b8ec5f106c3d38
Johandre97 <johandrehdb@gmail.com>
2023-10-07 23:11:37 +0200

    Connected contact form, some css changes, resized image

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import os
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from email.message import EmailMessage
+from string import Template
+import smtplib
 
 app = Flask(__name__)
 app.template_folder = './templates'
@@ -12,19 +15,27 @@ app.static_folder = './static'
 
 load_dotenv()
 
+#  Database set-up from environment variables
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_POOL_TIMEOUT'] = 3600  # 30 days * 24 hours * 60 minutes * 60 seconds
+
+# Database configuration
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 3600  # Set database time-out to 1 hour
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     'pool_pre_ping': True,  # Automatically reconnect on connection loss
 }
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-
+#  Initialize database with SQL Alchemy
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Load your SMTP email and password from environment variables
+smtp_email = os.getenv("SMTP_EMAIL")
+smtp_password = os.getenv("SMTP_PASSWORD")
 
+
+# Define a guest model for the database
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -32,13 +43,35 @@ class Guest(db.Model):
     guests_count = db.Column(db.Integer, nullable=False)
     message = db.Column(db.Text)
 
+# Send Email Function
+
+
+def send_email(subject, recipients, content, cc=None):
+    html_template = Template(content)
+    html_content = html_template.substitute()
+
+    message = EmailMessage()
+    message['from'] = 'Our Wedding - Mailer'
+    message['subject'] = subject
+    message.set_content(html_content, 'html')
+
+    message['to'] = recipients
+
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(smtp_email, smtp_password)
+        smtp.send_message(message)
+
 
 def current_page():
     return {'current_page': request.endpoint}
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/rsvp', methods=['POST'])
 def rsvp():
@@ -57,7 +90,8 @@ def rsvp():
                 return redirect(url_for('index'))
 
             # If the guest is not found, create a new guest record
-            new_guest = Guest(name=name, email=email, guests_count=guests_count, message=message)
+            new_guest = Guest(name=name, email=email,
+                              guests_count=guests_count, message=message)
 
             # Add the new guest to the session and commit the transaction
             db.session.add(new_guest)
@@ -67,10 +101,42 @@ def rsvp():
             return redirect(url_for('index'))
 
         except Exception as e:
-            flash('An error occurred while processing your reservation. Please try again later.', 'danger')
+            flash(
+                'An error occurred while processing your reservation. Please try again later.', 'danger')
             print(f"Error occurred: {str(e)}")
             return redirect(url_for('index'))
 
+# Contact form route
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        # Send an email to your desired recipients
+        # Replace with your email address or a list of recipients
+        recipients = ['johandrehdb@gmail.com', 'lnaude101@gmail.com']
+
+        # Construct the HTML content for the email
+        email_subject = 'Contact Form Submission'
+        email_content = f"Name: {name}<br>Email: {email}<br>Message: {message}"
+
+        try:
+            send_email(email_subject, recipients, email_content)
+
+            flash('Your message has been sent!', 'success')
+            return redirect(url_for('contact'))
+
+        except Exception as e:
+            flash(
+                'An error occurred while sending your message. Please try again later.', 'danger')
+            print(f"Error occurred: {str(e)}")
+
+    # Replace 'contact.html' with the actual template file
+    return render_template('contact.html')
 
 
 @app.route('/<string:page_name>')
