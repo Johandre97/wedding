824ece59ca3daa71f99bb8993284b86856e8593f
Johandre97 <johandrehdb@gmail.com>
2023-10-07 20:21:21 +0000

    Connected rsvp on accomodation, changed rsvp date. Changed catch block

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ load_dotenv()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 3600  # 30 days * 24 hours * 60 minutes * 60 seconds
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_pre_ping': True,  # Automatically reconnect on connection loss
+}
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
@@ -44,25 +48,29 @@ def rsvp():
         guests_count = request.form['guests_count']
         message = request.form['message']
 
-        # Check if the guest with the same email already exists
-        existing_guest = Guest.query.filter_by(email=email).first()
-
-        if existing_guest:
-            flash('You have already reserved with this email address.', 'danger')
-            return redirect(url_for('index'))
-
-        # If the guest is not found, create a new guest record
-        new_guest = Guest(name=name, email=email, guests_count=guests_count, message=message)
-
         try:
+            # Check if the guest with the same email already exists
+            existing_guest = Guest.query.filter_by(email=email).first()
+
+            if existing_guest:
+                flash('You have already reserved with this email address.', 'danger')
+                return redirect(url_for('index'))
+
+            # If the guest is not found, create a new guest record
+            new_guest = Guest(name=name, email=email, guests_count=guests_count, message=message)
+
+            # Add the new guest to the session and commit the transaction
             db.session.add(new_guest)
             db.session.commit()
+
             flash('Thank you for your reservation!', 'success')
-            return redirect(url_for('thankyou'))
+            return redirect(url_for('index'))
+
         except Exception as e:
             flash('An error occurred while processing your reservation. Please try again later.', 'danger')
             print(f"Error occurred: {str(e)}")
             return redirect(url_for('index'))
+
 
 
 @app.route('/<string:page_name>')
